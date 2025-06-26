b27ab12f5d2faee092b89083f7f6b651cc7e95f4
belamere03 <belamere03@gmail.com>
2025-06-26 22:39:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -331,3 +331,10 @@ def save_to_excel(survey):
 @app.get("/healthz")
 def health_check():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))  # Use Render's PORT or default to 8000
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
